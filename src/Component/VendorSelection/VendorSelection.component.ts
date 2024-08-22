2e136a46fd47cc5562984028f8d039f42cc798ad
yiungyiung <yashparkhi.super@gmail.com>
2024-08-22 23:21:31 +0530

    adding sel;ect all for vednorselceted

--- src/Component/VendorSelection/VendorSelection.component.ts
+++ src/Component/VendorSelection/VendorSelection.component.ts
@@ -1,6 +1,6 @@
-
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Vendor } from '../../model/vendor';
+
 @Component({
   selector: 'app-VendorSelection',
   templateUrl: './VendorSelection.component.html',
@@ -12,6 +12,7 @@ export class VendorSelectionComponent {
   @Output() vendorSelectionChanged = new EventEmitter<{ vendorID: number, isSelected: boolean }>();
 
   collapsedStates: { [key: number]: boolean } = {};
+  selectedVendors: { [vendorID: number]: boolean } = {}; // Track selected vendors
 
   toggleCollapse(index: number) {
     this.collapsedStates[index] = !this.collapsedStates[index];
@@ -25,6 +26,7 @@ export class VendorSelectionComponent {
     const isChecked = (event.target as HTMLInputElement).checked;
     category.vendors.forEach((vendor: Vendor) => {
       if (vendor.vendorID !== undefined) {
+        this.selectedVendors[vendor.vendorID] = isChecked;
         this.vendorSelectionChanged.emit({ vendorID: vendor.vendorID, isSelected: isChecked });
       }
     });
@@ -33,6 +35,7 @@ export class VendorSelectionComponent {
   onVendorSelectionChange(event: Event, vendor: Vendor) {
     const isChecked = (event.target as HTMLInputElement).checked;
     if (vendor.vendorID !== undefined) {
+      this.selectedVendors[vendor.vendorID] = isChecked;
       this.vendorSelectionChanged.emit({ vendorID: vendor.vendorID, isSelected: isChecked });
     }
   }
@@ -43,5 +46,10 @@ export class VendorSelectionComponent {
 
   isVendorDisabled(categoryID: number): boolean {
     return this.selectedCategoryID !== undefined && this.selectedCategoryID !== categoryID;
+  }
+
+  // Helper method to determine if all vendors in the category are selected
+  areAllVendorsSelected(category: any): boolean {
+    return category.vendors.every((vendor: Vendor) => this.selectedVendors[vendor.vendorID!] === true);
   }
 }
